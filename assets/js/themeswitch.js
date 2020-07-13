ed958715ca3f4f364433741d0bb776d90609300c
SebasWild <sebastianrwild@gmail.com>
2020-07-12 17:42:32 -0700

    Style progress bars on about page
    To make this possible, a few other things were changed:
    - Added default.html layout - this overrides the one provided by MM
    - Added color map & mixing to main.scss
    - Modified themeswitch.js to apply theme class to body tag

--- assets/js/themeswitch.js
+++ assets/js/themeswitch.js
@@ -20,16 +20,20 @@ window.matchMedia('(prefers-color-scheme: dark)').addListener(function (e) {
 function activateLightTheme() {
     const lightSheet = document.getElementById("sheet_light");
     const darkSheet = document.getElementById("sheet_dark");
+    const body = document.getElementsByTagName("BODY")[0];
 
 
     // lightSheet.media = '';
     darkSheet.media = 'none';
+    body.classList.replace("theme--dark", "theme--light");
 }
 
 function activateDarkTheme() {
     const lightSheet = document.getElementById("sheet_light");
     const darkSheet = document.getElementById("sheet_dark");
+    const body = document.getElementsByTagName("BODY")[0];
 
     // lightSheet.media = 'none';
     darkSheet.media = '';
+    body.classList.replace("theme--light", "theme--dark");
 }
